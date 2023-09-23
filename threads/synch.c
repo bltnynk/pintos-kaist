d3e6f1a9179327c5394ebab8994153e571aaf9c9
Thanh-Long V. Le <thanhlongtdk2000@gmail.com>
2023-09-23 23:33:21 +0900

    Only yield when there's no ongoing interrupts

--- threads/synch.c
+++ threads/synch.c
@@ -118,7 +118,9 @@ sema_up (struct semaphore *sema) {
 		struct thread *cur_thread = list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem);
 		thread_unblock (cur_thread);
-		thread_yield();
+		if (!intr_context()) {
+			thread_yield();
+		}
 	}
 	intr_set_level (old_level);
 }
@@ -325,8 +327,8 @@ cond_wait (struct condition *cond, struct lock *lock) {
 	ASSERT (lock_held_by_current_thread (lock));
 
 	sema_init (&waiter.semaphore, 0);
-	lock_release (lock);
 	list_push_back(&cond->waiters, &waiter.elem);
+	lock_release (lock);
 	sema_down (&waiter.semaphore);
 	lock_acquire (lock);
 }
